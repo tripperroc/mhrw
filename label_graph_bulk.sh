9cd3c2ec8fda09398dfa597a0c1e55243f760fbb
CHRISTOPHER M HOMAN <choman@bhc0043.bluehive.circ.private>
2015-02-12 12:12:34 -0500

    Changed slurm scripts a bit.

--- label_graph_bulk.sh
+++ label_graph_bulk.sh
@@ -5,15 +5,15 @@ do
     do
 	for ep in 1 1.25 1.5 1.75 2 2.25
 	do
-	    name=gd
+	    for name in gd
             do
-		for init in 2 3 4
-		do
-		    prop in 0 1
-                    do
+#		for init in 2 3 4
+#		do
+#		    prop in 0 1
+#                   do
 			sbatch -o c_${d}_${k}_${ep}_${name}_${init}_${prop}.out -e c_${d}_${k}_${ep}_${name}_${init}_${prop}.err label_graph.sh ${d} ${k} ${ep}_${k} ${ep} ${name} ${init} ${prop}
-                    done
-                 done
+#                    done
+#                 done
 	    done
 	done
     done
